5b1a8a7175050fdcdd799abb1ae60f1a415d6b9c
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-06-25 11:47:25 -0700

    If applied, commit will remove server.js

--- index.js
+++ index.js
@@ -1,40 +1,165 @@
 const express = require("express"),
-  morgan = require("morgan"),
-  path = require("path");
-const app = express();
+  app = express(),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
-//movie list
-let topMovies = [
-  { title: "John Wick" },
-  { title: "John Wick 2" },
-  { title: "John Wick 3: Parabellum" },
-  { title: "John Wick: Chapter 4" },
-  { title: "Pokemon The first Movie" },
-  { title: "Rush Hour" },
-  { title: "Rush Hour 2" },
-  { title: "Rush Hour 3" },
-  { title: "Harry Potter and the Sorcerer's Stone" },
-  { title: "WALLE" },
+app.use(bodyParser.json());
+
+let users = [
+  {
+    id: 1,
+    name: "Anthony",
+    favoriteMovies: [],
+  },
+  { id: 2, name: "Joe", favoriteMovies: ["The Fountain"] },
 ];
 
-app.use(morgan("common"));
-app.use(express.static("public"));
+let movies = [
+  {
+    Title: "The Fountain",
+    Description:
+      "as a modern-day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.",
+    Genre: {
+      Name: "Drama",
+      Description:
+        "In film and television, drama is a category of narrative fiction (or semi-fiction) intented to be more seriousthan humorous in tone.",
+    },
+    Director: {
+      Name: "Darren Aronofsky",
+      Bio: "Darren Aronofsky was born Februrary 12, 1969, in Brooklyn, New York.",
+      Birth: 1969,
+    },
+    ImageURL: "some website",
+    Featured: false,
+  },
+  {
+    Title: "Example",
+    Description: "Example",
+    Genre: {
+      Name: "Thriller",
+      Description: "Thriller Example.",
+    },
+    Director: {
+      Name: "Example Person",
+      Bio: "Person Example.",
+      Birth: 1969,
+    },
+    ImageURL: "some website",
+    Featured: false,
+  },
+];
 
-//endpoint requests
-app.get("/", (req, res) => {
-  res.send("Welcome to my movie app!");
+//CREATE
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("Users needs name");
+  }
 });
 
+//UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//CREATE
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+  if (user) {
+    user = users.filter((user) => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//READ
 app.get("/movies", (req, res) => {
-  res.send(topMovies);
+  res.status(200).json(movies);
 });
 
-//error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke! Try again later!");
+//READ
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
 });
 
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+//READ
+app.get("/movies/genres/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre");
+  }
 });
+
+//READ
+app.get("/movies/directors/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.Director.Name === directorName
+  ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director");
+  }
+});
+
+app.listen(8080, () => console.log("listening on 8080"));
