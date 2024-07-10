1566d8bfa64ce0157deeed008b31fc27a71f5077
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-07-10 12:25:11 -0700

    If applied, commit will add mongoose

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   uuid = require("uuid");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://localhost:27017/cfdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 
